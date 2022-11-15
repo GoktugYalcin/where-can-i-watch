eb64a26a298e48a4f86534f8f36c35160167df6d
GoktugYalcin <goktugyalcin06@gmail.com>
2022-11-16 00:30:46 +0300

    Initial commit - not completed
    It includes the select and fetch by rules. Listing will be come soon.

---
+++ README.md
@@ -0,0 +1,24 @@
+## Where Can I Watch?
+
+This app can provide the channel list that you can buy/rent/watch for free.
+
+For development purpose,
+
+- Firstly, you need the packages that used in project.
+```shell
+yarn
+```
+
+or 
+```shell
+npm install
+```
+
+- Then you can start it with,
+```shell
+yarn dev
+```
+or
+```shell
+npm run dev
+```
---
+++ pages/api/search/[searchType].ts
@@ -0,0 +1,59 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
+
+
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    const parseQuery = (query: any): (string | boolean) => {
+        const BASE_URL = "https://api.themoviedb.org/3";
+        const api = axios.create({ baseURL: BASE_URL })
+        const key = process.env.TMDB_API_KEY
+        const { searchType } = req.query
+
+        if(searchType === "multi") {
+            api.get("search/multi", {
+                params: {
+                    api_key: key,
+                    language: 'en-US',
+                    query: query,
+                    page: 1,
+                    include_adult: false
+                },
+            }).then(response => {
+                res.status(200).json(response.data.results)
+            }).catch((err): void => {
+                if (axios.isCancel(err)) {
+                    console.log('Previous request canceled, new request is send', err.message);
+                }
+                else {
+                    console.log(err.message)
+                }
+            })
+        }
+        else if(searchType === "country") {
+            api.get(`${query.media_type}/${query.id}/watch/providers`, {
+                params: {
+                    api_key: key
+                },
+            }).then(response => {
+                res.status(200).json(response.data.results)
+            }).catch((err: Error): void => {
+                if (axios.isCancel(err)) {
+                    console.log('Previous request canceled, new request is send', err.message);
+                }
+                else {
+                    console.log(err)
+                }
+            })
+        }
+
+
+        return true
+    }
+
+    parseQuery(req.body.query)
+}
