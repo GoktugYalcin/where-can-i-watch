de6be4ed4836c7b63663e07d6b8cdf9e4cbc9e51
GoktugYalcin <goktugyalcin06@gmail.com>
2024-08-05 20:29:25 +0300

    feat: Added vercel features

--- app/fetchCountry/route.ts
+++ app/fetchCountry/route.ts
@@ -2,12 +2,7 @@ import { NextRequest } from "next/server";
 import ky from "ky";
 
 export async function GET(request: NextRequest) {
-  const res = await ky(
-    `https://api.country.is/${(request.headers.get("x-forwarded-for") as string) ?? ""}`,
-  ).json<{
-    country: string;
-  }>();
   return Response.json({
-    ...res,
+    country: request.headers.get("X-Vercel-IP-Country") ?? "FR",
   });
 }
