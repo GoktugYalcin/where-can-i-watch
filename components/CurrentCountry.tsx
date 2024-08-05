0cdb87b23153d61aa074affa65e5cf41ac4373f2
GoktugYalcin <goktugyalcin06@gmail.com>
2024-08-05 20:39:19 +0300

    feat: Request got back to client

--- components/CurrentCountry.tsx
+++ components/CurrentCountry.tsx
@@ -1,37 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import ky from "ky";
 import { AnimatedShinyTextWrapper } from "@/components/AnimatedShinyTextWrapper";
 import CountryWrapper from "@/components/CountryWrapper";
 
 export async function CurrentCountry() {
-  let country;
-  try {
-    const res = await ky("https://api.country.is/").json<{
-      country: string;
-    }>();
-    country = res.country as string;
-  } catch (e) {
-    return (
+  const [country, setCountry] = useState<string | null>(null);
+
+  useEffect(() => {
+    ky("https://api.country.is/")
+      .json<{
+        country: string;
+      }>()
+      .then((res) => setCountry(res.country));
+  }, []);
+
+  return !!country ? (
+    <>
       <span className="sticky w-fit ml-12 bottom-5">
         <AnimatedShinyTextWrapper>
-          <span>Loading...</span>
+          <span className="flex justify-center items-center gap-2">
+            <span>You are connecting from:</span>
+            <CountryWrapper country={country} />
+          </span>
         </AnimatedShinyTextWrapper>
       </span>
-    );
-  }
-  return (
-    !!country && (
-      <>
-        <span className="sticky w-fit ml-12 bottom-5">
-          <AnimatedShinyTextWrapper>
-            <span className="flex justify-center items-center gap-2">
-              <span>You are connecting from:</span>
-              <CountryWrapper country={country} />
-            </span>
-          </AnimatedShinyTextWrapper>
-        </span>
-      </>
-    )
+    </>
+  ) : (
+    <span className="sticky w-fit ml-12 bottom-5">
+      <AnimatedShinyTextWrapper>
+        <span>Loading...</span>
+      </AnimatedShinyTextWrapper>
+    </span>
   );
 }
 
