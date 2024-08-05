2c9a10cad39d3486c31ced6bbcff75ab4ad5a0c5
GoktugYalcin <goktugyalcin06@gmail.com>
2024-08-05 20:25:59 +0300

    fix: Fix fix fix

--- app/fetchCountry/route.ts
+++ app/fetchCountry/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest } from "next/server";
+import ky from "ky";
 
 export async function GET(request: NextRequest) {
+  const res = await ky(
+    `https://api.country.is/${(request.headers.get("x-forwarded-for") as string) ?? ""}`,
+  ).json<{
+    country: string;
+  }>();
   return Response.json({
-    country: request.headers.get("x-forwarded-for") as string,
+    ...res,
   });
 }
